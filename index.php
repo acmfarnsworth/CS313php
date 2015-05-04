e38ce5725b6305a77e96e237a1b5dbd710d0044d
Angela <angelamulberg@gmail.com>
2015-05-04 13:47:27 -0700

    commit this

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
+<!DOCTYPE html>
 <html>
-	
-	<body>
-<h1>Hello World!</h1>
+ <head>
+ <meta charset="utf-8">
+ <title>Angela Mulberg | CS313</title>
+ <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/head.php'; ?>
+ </head>
+ 
+ 	<header>
+	 	<?php include $_SERVER['DOCUMENT_ROOT'].'/modules/header.php'; ?>
+ 	</header>
+   
+ 	<nav>
+	 	<?php include $_SERVER['DOCUMENT_ROOT'].'/modules/nav.php'; ?>
+ 	</nav>
+   
+   	<main>
+	   	<p>Hello and welcome to my little section of the web! I am creating this site for my class CS 313 and am excited to learn and develop!</p>
 
-<p>Having fun using the terminal! </p>
-	</body>
+<p><a href="http://validator.w3.org/check?uri=http%3A%2F%2Fcdesignmechanical.com%2F">Validated</a> by W3C</p>
+	</main>
+   
+<footer>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/modules/footer.php'; ?>
+</footer>
+
+</body>
 </html>
