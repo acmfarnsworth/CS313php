865d30aa29915ec8e2a09429e0e21a1e555c1720
Angela <angelamulberg@gmail.com>
2015-05-04 14:40:39 -0700

    hoping this works

---
+++ modules/header.php
@@ -0,0 +1,1 @@
+<h1>Silivrenwolf Photography</h1>
---
+++ modules/nav.php
@@ -0,0 +1,9 @@
+<ul>
+	<li><a href="index.php">Home</a></li>
+	<li><a href="about.php">About</a></li>
+	<li><a href="equines.php">Equines</a></li>
+	<li><a href="pets.php">Pets</a></li>
+	<li><a href="events.php">Events</a></li>
+	<li><a href="contact.php">Contact</a></li>
+	<li><a href="assignments.php">Assignments</a></li>
+</ul>
